b2717228dd33c92065b65958443984416be982d9
Claveria <claveriaency@gmail.com>
2026-02-21 17:12:05 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+namespace cabillocalendar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---CABILLO CALENDAR MANAGEMENT SYSTEM---");
+            Console.Write("Enter a date: ");
+            string DateTime = Console.ReadLine();
+            Console.Write("Enter your event for this day: ");
+            string eventday = Console.ReadLine();
+
+
+            String[] titles = new string[10];
+            DateTime[] starts = new DateTime [10];
+            DateTime[] ends = new DateTime[10];
+            int eventCount = 0;
+
+            bool isRunning = true;
+
+
+
+
+        }
+    }
+}
